d21361ecc5f1d2b61e93240b95459ae29893270b
stankis <151746694+stankis@users.noreply.github.com>
2024-01-18 21:48:55 +0300

    Added sorting by aliases in favorites (#57)
    * Added sorting by aliases in favorites
    
    * Added script to update alias toLowerCase in favorites
    
    * Code review fixes
    
    * Updated alias migration script for favorites
    
    * Code review fixes
    
    * Update update-alias-favorites.ts
    
    * Update update-alias-favorites.ts
    
    * Code review fixes
    
    * Code review fixes
    
    * Added sorting to aliases
    
    ---------
    
    Co-authored-by: Stanislav Kiselev <“staskis@yandex-team.ru”>
    Co-authored-by: Maksim Sitnikov <msitnikov1@yandex-team.ru>

---
+++ src/db/scripts/update-alias-favorites.ts
@@ -0,0 +1,39 @@
+/* eslint-disable camelcase */
+/* This script facilitates migration of aliases for favorites and can be deleted after release */
+require('dotenv').config();
+require('../../index');
+import {db} from '../index';
+
+(async function () {
+    try {
+        await db.ready();
+
+        await db.primary.raw(
+            `UPDATE favorites 
+             SET display_alias = NULL 
+             WHERE display_alias IS NOT NULL AND display_alias != '' AND alias IS NULL`,
+        );
+
+        const favorites = await db.primary.raw(
+            `SELECT entry_id, tenant_id, login, alias, display_alias 
+             FROM favorites 
+             WHERE alias IS NOT NULL AND alias != ''`,
+        );
+
+        for (const entry of favorites.rows) {
+            const {entry_id, tenant_id, login, alias, display_alias} = entry;
+
+            if (display_alias === null || alias.toLowerCase() !== display_alias.toLowerCase()) {
+                await db.primary
+                    .table('favorites')
+                    .update({alias: alias.toLowerCase(), display_alias: alias})
+                    .where({entry_id, tenant_id, login});
+            }
+        }
+
+        process.exit(0);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+})();
