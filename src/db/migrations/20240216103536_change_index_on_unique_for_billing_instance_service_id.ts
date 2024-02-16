f80cef3746aed42bb7fbc4b175a250773a61b3fd
Sergei Samokhvalov <samokhvalovse@yandex-team.ru>
2024-02-16 15:49:42 +0300

    Delete renamin index

--- src/db/migrations/20240216103536_change_index_on_unique_for_billing_instance_service_id.ts
+++ src/db/migrations/20240216103536_change_index_on_unique_for_billing_instance_service_id.ts
@@ -2,32 +2,22 @@ import {Knex} from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     return knex.raw(`
-        CREATE UNIQUE INDEX CONCURRENTLY
-            tenants_new_billing_instance_service_id_idx
-        ON tenants USING
-            BTREE (billing_instance_service_id);
-  
         DROP INDEX CONCURRENTLY tenants_billing_instance_service_id_idx;
-  
-        ALTER INDEX
-            tenants_new_billing_instance_service_id_idx
-        RENAME TO
-            tenants_billing_instance_service_id_idx;
+
+        CREATE UNIQUE INDEX CONCURRENTLY
+            tenants_billing_instance_service_id_idx
+        ON tenants USING
+            BTREE (billing_instance_service_id);      
     `);
 }
 
 export async function down(knex: Knex): Promise<void> {
     return knex.raw(`
+        DROP INDEX CONCURRENTLY tenants_billing_instance_service_id_idx;
+        
         CREATE INDEX CONCURRENTLY
             tenants_new_billing_instance_service_id_idx
         ON tenants USING
             BTREE (billing_instance_service_id);
-  
-        DROP INDEX CONCURRENTLY tenants_billing_instance_service_id_idx;
-  
-        ALTER INDEX
-            tenants_new_billing_instance_service_id_idx
-        RENAME TO
-            tenants_billing_instance_service_id_idx;
     `);
 }
