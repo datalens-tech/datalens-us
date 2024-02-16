62186f02028b2a3cda30bc9744fc69305c5e4113
Sergei Samokhvalov <samokhvalovse@yandex-team.ru>
2024-02-16 16:18:19 +0300

    Change name index

--- src/db/migrations/20240216103536_change_index_on_unique_for_billing_instance_service_id.ts
+++ src/db/migrations/20240216103536_change_index_on_unique_for_billing_instance_service_id.ts
@@ -16,7 +16,7 @@ export async function down(knex: Knex): Promise<void> {
         DROP INDEX CONCURRENTLY tenants_billing_instance_service_id_idx;
         
         CREATE INDEX CONCURRENTLY
-            tenants_new_billing_instance_service_id_idx
+            tenants_billing_instance_service_id_idx
         ON tenants USING
             BTREE (billing_instance_service_id);
     `);
