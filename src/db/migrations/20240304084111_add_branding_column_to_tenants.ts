f81ceec5ad1332bb2db54570e612a5538e768ee1
Stanislav Kiselev <staskis@yandex-team.ru>
2024-03-04 11:59:43 +0300

    Added branding column to tenants table

--- src/db/migrations/20240304084111_add_branding_column_to_tenants.ts
+++ src/db/migrations/20240304084111_add_branding_column_to_tenants.ts
@@ -2,7 +2,7 @@ import type {Knex} from 'knex';
 
 exports.up = function (knex: Knex): Promise<unknown> {
     return knex.raw(`
-        ALTER TABLE tenants ADD COLUMN branding jsonb DEFAULT NULL;
+        ALTER TABLE tenants ADD COLUMN branding jsonb DEFAULT '{}'::jsonb;
     `);
 };
 
