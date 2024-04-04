6178ca4b8d3082601b6314ab1d1f3f3f41a0fdc8
Maksim Sitnikov <msitnikov1@yandex-team.ru>
2024-04-04 14:16:45 +0300

    Remove data from CREATE_STATE_REQUEST log (#106)

--- src/services/new/state/create-state.ts
+++ src/services/new/state/create-state.ts
@@ -33,7 +33,7 @@ export const createState = async (
 
     const {tenantId} = ctx.get('info');
 
-    logInfo(ctx, 'CREATE_STATE_REQUEST', {tenantId, entryId, data});
+    logInfo(ctx, 'CREATE_STATE_REQUEST', {tenantId, entryId});
 
     if (!skipValidation) {
         validateArgs(args);
