6241d6031fafc3a2e32288ac3b71fdc683fc3ee5
stankis <151746694+stankis@users.noreply.github.com>
2024-06-24 02:25:23 +0300

    Add Zitadel user roles

--- src/utils/zitadel.ts
+++ src/utils/zitadel.ts
@@ -1,28 +1,54 @@
 import {AppContext} from '@gravity-ui/nodekit';
-import {nodekit} from '../nodekit';
 import {Utils} from './utils';
 import axios from 'axios';
 import axiosRetry from 'axios-retry';
+
+enum ZitadelUserRole {
+    Creator = 'Creator',
+    Admin = 'Admin',
+    Viewer = 'Viewer',
+}
 
 type IntrospectionResult = {
     active: boolean;
     userId?: string;
     username?: string;
+    role?: ZitadelUserRole;
 };
 
 const axiosInstance = axios.create();
 axiosRetry(axiosInstance, {retries: 3});
 
+const getRole = (data: any): ZitadelUserRole => {
+    const scope = 'urn:zitadel:iam:org:project:roles';
+
+    const roles = data[scope];
+
+    if (!roles) {
+        return ZitadelUserRole.Viewer;
+    }
+
+    if (roles['admin']) {
+        return ZitadelUserRole.Admin;
+    }
+
+    if (roles['creator']) {
+        return ZitadelUserRole.Creator;
+    }
+
+    return ZitadelUserRole.Viewer;
+};
+
 export const introspect = async (ctx: AppContext, token?: string): Promise<IntrospectionResult> => {
     ctx.log('Token introspection');
 
-    if (!nodekit.config.zitadelUri) {
+    if (!ctx.config.zitadelUri) {
         throw new Error('Missing ZITADEL_URI in env');
     }
-    if (!nodekit.config.clientId) {
+    if (!ctx.config.clientId) {
         throw new Error('Missing CLIENT_ID in env');
     }
-    if (!nodekit.config.clientSecret) {
+    if (!ctx.config.clientSecret) {
         throw new Error('Missing CLIENT_SECRET in env');
     }
 
@@ -35,10 +61,10 @@ export const introspect = async (ctx: AppContext, token?: string): Promise<Intro
 
         const response = await axiosInstance({
             method: 'post',
-            url: `${nodekit.config.zitadelUri}/oauth/v2/introspect`,
+            url: `${ctx.config.zitadelUri}/oauth/v2/introspect`,
             auth: {
-                username: nodekit.config.clientId,
-                password: nodekit.config.clientSecret,
+                username: ctx.config.clientId,
+                password: ctx.config.clientSecret,
             },
             params: {
                 token,
@@ -48,7 +74,10 @@ export const introspect = async (ctx: AppContext, token?: string): Promise<Intro
         ctx.log(`Token introspected successfully within: ${Utils.getDuration(hrStart)} ms`);
 
         const {active, username, sub} = response.data;
-        return {active: Boolean(active), userId: sub, username};
+
+        const role = getRole(response.data);
+
+        return {active: Boolean(active), userId: sub, username, role};
     } catch (e) {
         ctx.logError('Failed to introspect token', e);
         return {active: false};
