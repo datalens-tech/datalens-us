393615bdba5a8b90d950a61bcfe6a400095ecdc3
Maksim Sitnikov <msitnikov1@yandex-team.ru>
2024-08-16 12:20:42 +0300

    Add embed fields (#170)
    * Add embed fields
    
    * Add new columns to JoinedEmbedEmbeddingSecret

--- src/db/models/new/embed/index.ts
+++ src/db/models/new/embed/index.ts
@@ -7,6 +7,8 @@ export const EmbedModelColumn = {
     EntryId: 'entryId',
     DepsIds: 'depsIds',
     UnsignedParams: 'unsignedParams',
+    PrivateParams: 'privateParams',
+    PublicParamsMode: 'publicParamsMode',
     CreatedBy: 'createdBy',
     CreatedAt: 'createdAt',
 } as const;
@@ -26,6 +28,8 @@ export class EmbedModel extends Model {
     [EmbedModelColumn.EntryId]!: string;
     [EmbedModelColumn.DepsIds]!: string[];
     [EmbedModelColumn.UnsignedParams]!: string[];
+    [EmbedModelColumn.PrivateParams]!: string[];
+    [EmbedModelColumn.PublicParamsMode]!: boolean;
     [EmbedModelColumn.CreatedBy]!: string;
     [EmbedModelColumn.CreatedAt]!: string;
 }
