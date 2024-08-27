b55d62104f38f59490a6730c154fa775b5fe8400
thexardas <thexardas@yandex-team.ru>
2024-08-27 17:45:29 +0300

    Review fix: set updated_at values from created_at

--- src/db/migrations/20240821143300_embeds_change_updated_by_to_not_null.ts
+++ src/db/migrations/20240821143300_embeds_change_updated_by_to_not_null.ts
@@ -2,7 +2,7 @@ import {Knex} from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     return knex.raw(`
-        UPDATE embeds SET updated_by = created_by;
+        UPDATE embeds SET updated_by = created_by, updated_at = created_at;
         ALTER TABLE embeds ALTER COLUMN updated_by SET NOT NULL;
     `);
 }
