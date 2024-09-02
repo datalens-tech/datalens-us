4f095e7a662c5ba59fa7f86aa032ef8b1246b2ff
thexardas <thexardas@yandex-team.ru>
2024-09-02 17:47:44 +0300

    Review fix, remove redundant service methods

--- api/services.ts
+++ api/services.ts
@@ -38,9 +38,3 @@ export {
 export {formatWorkbookModel} from '../src/services/new/workbook/formatters';
 
 export {crossSyncCopiedJoinedEntryRevisions} from '../src/services/new/workbook';
-
-export {
-    getTenantByEntryId,
-    getTenantByWorkbookId,
-    getTenantByCollectionId,
-} from '../src/services/new/tenant';
--- src/services/new/tenant/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './get-tenant-by-entry-id';
-export * from './get-tenant-by-collection-id';
-export * from './get-tenant-by-workbook-id';
