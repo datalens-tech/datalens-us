f4933d97c2c853e63fc200b5823cda7dd3095c72
Maksim Sitnikov <msitnikov1@yandex-team.ru>
2024-09-09 16:44:00 +0300

    Fix locale for comments migration

--- src/db/migrations/20240903191030_comments_add_lower_feed_and_date_until_indexes.ts
+++ src/db/migrations/20240903191030_comments_add_lower_feed_and_date_until_indexes.ts
@@ -2,7 +2,15 @@ import {Knex} from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     return knex.raw(`
-        CREATE INDEX comments_feed_lower_is_removed_date_date_until_idx ON comments (is_removed, lower(feed collate "en_US.utf8"), date, date_until);
+        DO $$
+            BEGIN
+            IF EXISTS (SELECT collname FROM pg_collation WHERE collname = 'en_US.utf8') THEN
+                CREATE INDEX comments_feed_lower_is_removed_date_date_until_idx ON comments (is_removed, lower(feed collate "en_US.utf8"), date, date_until);
+            ELSE
+                CREATE INDEX comments_feed_lower_is_removed_date_date_until_idx ON comments (is_removed, lower(feed), date, date_until);
+            END IF;
+        END $$;
+
         CREATE INDEX comments_date_until_idx ON comments (date_until);
     `);
 }
