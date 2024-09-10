2317c134d5bb646ab44e37d64bd7d3c52efa333b
Artem Panchuk <apanchuk@yandex-team.ru>
2024-09-10 15:21:38 +0300

    Change migration

--- src/db/migrations/20240910124901_rename_presentation_to_report.ts
+++ src/db/migrations/20240910124901_rename_presentation_to_report.ts
@@ -2,12 +2,14 @@ import {Knex} from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     return knex.raw(`
-        ALTER TYPE scope ADD VALUE 'report';
+        ALTER TYPE scope RENAME VALUE 'presentation' TO 'report';
     `);
 }
 
 export async function down(knex: Knex): Promise<void> {
-    return knex.raw('');
+    return knex.raw(`
+        ALTER TYPE scope RENAME VALUE 'report' TO 'presentation';
+    `);
 }
 
 export const config = {
