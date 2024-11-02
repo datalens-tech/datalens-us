e99d311420e30cbde9c5366a232fd8b451b0b06e
Sergei Samokhvalov <samokhvalovse@yandex-team.ru>
2024-11-02 12:40:14 +0300

    Add to registry method markWorkbooksAsDeleted

--- src/registry/common/entities/workbook/utils.ts
+++ src/registry/common/entities/workbook/utils.ts
@@ -1,3 +1,8 @@
+import {raw} from 'objection';
+import {CURRENT_TIMESTAMP} from '../../../../const';
+import {WorkbookModel, WorkbookModelColumn} from '../../../../db/models/new/workbook';
+import {ServiceArgs} from '../../../../services/new/types';
+import {getPrimary} from '../../../../services/new/utils';
 import type {BulkFetchWorkbooksAllPermissions} from './types';
 import {Workbook} from './workbook';
 
@@ -14,4 +19,25 @@ export const bulkFetchWorkbooksAllPermissions: BulkFetchWorkbooksAllPermissions 
         }
         return workbook;
     });
+};
+
+export const markWorkbooksAsDeleted = async (
+    {ctx, trx}: ServiceArgs,
+    {workbookIds}: {workbookIds: string[]},
+) => {
+    const {
+        user: {userId},
+    } = ctx.get('info');
+
+    await WorkbookModel.query(getPrimary(trx))
+        .patch({
+            [WorkbookModelColumn.DeletedBy]: userId,
+            [WorkbookModelColumn.DeletedAt]: raw(CURRENT_TIMESTAMP),
+        })
+        .whereIn([WorkbookModelColumn.WorkbookId], workbookIds)
+        .returning('*')
+        .timeout(WorkbookModel.DEFAULT_QUERY_TIMEOUT);
+
+    if (ctx.config.accessServiceEnabled) {
+    }
 };
