420accd61908b5ad1b3694b597f9986bfd98b859
Evgeniy Shangin <shangin@yandex-team.ru>
2024-11-15 16:55:27 +0300

    Delete tenant migration

--- src/db/presentations/index.ts
+++ src/db/presentations/index.ts
@@ -1,6 +1,4 @@
 export * from './joined-entry-revision';
 export * from './joined-entry-revision-favorite';
-export * from './joined-migration-tenant';
-export * from './joined-entry-migration-tenant';
 export * from './joined-embed-embedding-secret';
 export * from './joined-entry-favorite';
