e4fe649f019bdbb9c833b280b204ec903e64d40b
Evgeniy Shangin <shangin@yandex-team.ru>
2024-11-18 10:25:45 +0300

    migration down create table

--- src/db/migrations/20241115145845_drop_table_migrations_tenants.ts
+++ src/db/migrations/20241115145845_drop_table_migrations_tenants.ts
@@ -6,6 +6,15 @@ export async function up(knex: Knex): Promise<void> {
     `);
 }
 
-export async function down(): Promise<void> {
-    return Promise.resolve();
+export async function down(knex: Knex): Promise<void> {
+    return knex.raw(`
+        CREATE TABLE migrations_tenants (
+            from_id TEXT NOT NULL,
+            to_id TEXT NOT NULL,
+            migrating BOOLEAN NOT NULL DEFAULT TRUE,
+            migration_meta JSONB NOT NULL DEFAULT '{}'::jsonb,
+            created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+            PRIMARY KEY (from_id, to_id)
+        );
+    `);
 }
