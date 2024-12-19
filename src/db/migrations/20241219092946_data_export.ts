c5ea22da67b02dc56a3b068bfaf3a0026b3fb443
Samat Jorobekov <samat1337@yandex-team.ru>
2024-12-19 13:36:03 +0300

    Add data export migration

---
+++ src/db/migrations/20241219092946_data_export.ts
@@ -0,0 +1,30 @@
+import type {Knex} from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.raw(`
+        CREATE TABLE data_export (
+            data_export_id BIGINT NOT NULL PRIMARY KEY DEFAULT get_id(),
+            title TEXT NOT NULL,
+            tenant_id TEXT,
+            chart_id BIGINT NOT NULL,
+            chart_rev_id BIGINT NOT NULL,
+            dataset_id BIGINT,
+            dataset_rev_id BIGINT NOT NULL,
+            connection_id BIGINT NOT NULL,
+            connection_rev_id BIGINT NOT NULL,
+            params JSONB NOT NULL DEFAULT '{}'::jsonb,
+            created_by TEXT NOT NULL,
+            created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+            expired_at TIMESTAMPTZ NOT NULL,
+            job_id TEXT NOT NULL,
+            result_link TEXT,
+            error JSONB
+        );
+    `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.raw(`
+        DROP TABLE data_export;
+    `);
+}
