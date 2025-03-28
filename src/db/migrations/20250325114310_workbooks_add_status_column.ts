0852f0895a3ffd252f6604403958684ad85b8093
Vladimir Stepanenko <endurance@yandex-team.ru>
2025-03-28 17:37:09 +0300

    Add workbook status migration

--- src/db/migrations/20250325114310_workbooks_add_status_column.ts
+++ src/db/migrations/20250325114310_workbooks_add_status_column.ts
@@ -2,9 +2,9 @@ import type {Knex} from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     return knex.raw(`
-        CREATE TYPE WORKBOOK_STATUS_ENUM AS ENUM ('importing');
+        CREATE TYPE WORKBOOK_STATUS_ENUM AS ENUM ('active', 'creating', 'deleting', 'deleted');
 
-        ALTER TABLE workbooks ADD COLUMN status WORKBOOK_STATUS_ENUM DEFAULT NULL;
+        ALTER TABLE workbooks ADD COLUMN status WORKBOOK_STATUS_ENUM DEFAULT 'active' NOT NULL;
     `);
 }
 
