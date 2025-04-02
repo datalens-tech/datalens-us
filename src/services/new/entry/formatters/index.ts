c4c23857246ec78dbb7d8e22706442dab1003b4a
Samat Jorobekov <samat1337@yandex-team.ru>
2025-04-02 21:16:03 +0300

    Add unversioned data to entries listing endpoint (#289)
    * Add unversionedData to entries listing endpoint
    
    * Return unversionedData on entries list endpoint

--- src/services/new/entry/formatters/index.ts
+++ src/services/new/entry/formatters/index.ts
@@ -4,3 +4,4 @@ export * from './format-get-entry-meta-private-response';
 export * from './format-get-entry-by-key-response';
 export * from './format-entry-model';
 export * from './format-joined-entry-revision-model';
+export * from './format-get-entries-response';
