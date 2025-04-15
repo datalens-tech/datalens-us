3b7cc939bf7f4fbd50a38c98ba907d8ea13456c0
Georgy Malkov <malkovgv@yandex-team.ru>
2025-04-15 15:58:41 +0300

    Enable readonly mode from env

--- src/configs/opensource/common.ts
+++ src/configs/opensource/common.ts
@@ -2,9 +2,10 @@ import {AppConfig} from '@gravity-ui/nodekit';
 
 import {Feature, FeaturesConfig} from '../../components/features/types';
 import {DL_SERVICE_USER_ACCESS_TOKEN} from '../../const';
+import {isTrueArg} from '../../utils/env-utils';
 
 export const features: FeaturesConfig = {
-    [Feature.ReadOnlyMode]: false,
+    [Feature.ReadOnlyMode]: isTrueArg(process.env.READ_ONLY_MODE),
     [Feature.CollectionsEnabled]: true,
     [Feature.ColorPalettesEnabled]: true,
     [Feature.UseIpV6]: false,
