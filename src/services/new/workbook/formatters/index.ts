bd86433cc2ccb03a25cea42a0c2c113771da3dad
Vladimir Stepanenko 🏴‍☠️ <endurance@yandex-team.ru>
2025-04-18 14:04:10 +0300

    Remove copy template endpoint (#294)
    * Remove copy template endpoint
    
    * Remove copy template endpoint

--- src/services/new/workbook/formatters/index.ts
+++ src/services/new/workbook/formatters/index.ts
@@ -1,7 +1,6 @@
 export * from './format-workbook';
 export * from './format-workbooks-list';
 export * from './format-workbook-model';
-export * from './format-workbook-model-with-operation';
 export * from './format-workbook-models-list';
 export * from './format-set-workbook-is-template';
 export * from './format-restore-workbook';
