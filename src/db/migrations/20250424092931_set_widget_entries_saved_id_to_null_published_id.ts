7872ba1f8e4af6bde789027bf4cfade98e2b9098
Vladimir Stepanenko <endurance@yandex-team.ru>
2025-04-24 14:46:23 +0300

    Add migration to set saved_id valude to widget entries with null published_id

--- src/db/migrations/20250424092931_set_widget_entries_saved_id_to_null_published_id.ts
+++ src/db/migrations/20250424092931_set_widget_entries_saved_id_to_null_published_id.ts
@@ -8,6 +8,6 @@ export async function up(knex: Knex): Promise<void> {
     `);
 }
 
-export async function down(knex: Knex): Promise<void> {
-    return knex.raw('');
+export async function down(): Promise<void> {
+    return;
 }
