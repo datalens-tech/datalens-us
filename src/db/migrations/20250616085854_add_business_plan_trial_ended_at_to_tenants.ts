f6d96da6d9f1807bb3c0c0f0421fcbee61f8f559
Vladimir Stepanenko <endurance@yandex-team.ru>
2025-06-16 20:13:40 +0300

    Fix naming

--- src/db/migrations/20250616085854_add_business_plan_trial_ended_at_to_tenants.ts
+++ src/db/migrations/20250616085854_add_business_plan_trial_ended_at_to_tenants.ts
@@ -2,12 +2,12 @@ import type {Knex} from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     return knex.raw(`
-        ALTER TABLE tenants ADD COLUMN business_plan_trial_ended_at TIMESTAMPTZ DEFAULT NULL;
+        ALTER TABLE tenants ADD COLUMN trial_ended_at TIMESTAMPTZ DEFAULT NULL;
     `);
 }
 
 export async function down(knex: Knex): Promise<void> {
     return knex.raw(`
-        ALTER TABLE tenants DROP COLUMN business_plan_trial_ended_at;
+        ALTER TABLE tenants DROP COLUMN trial_ended_at;
     `);
 }
