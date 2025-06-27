54c1620dd5516b406c69a04d8ae139ed4c195aa2
Yankin Anton <33173059+YankinA@users.noreply.github.com>
2025-06-27 15:12:59 +0300

    Remove env file usage in integration tests (#316)
    * remove env file usage in integration tests

---
+++ src/const/testing-env.ts
@@ -0,0 +1,8 @@
+export const TESTING_ENV = {
+    APP_INSTALLATION: 'opensource',
+    APP_ENV: 'int-testing',
+    ZITADEL: 'true',
+    MASTER_TOKEN: 'int-testing-master-token',
+    APP_LOGGING_LEVEL: 'silent',
+    US_SURPRESS_DB_STATUS_LOGS: 'true',
+};
