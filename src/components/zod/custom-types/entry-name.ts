3b4201f08f310d472a12beae8533679760f39800
Sergei Mulyk <s-mulyk@yandex-team.ru>
2025-08-14 16:01:19 +0300

    DLAPI-379: review changes

--- src/components/zod/custom-types/entry-name.ts
+++ src/components/zod/custom-types/entry-name.ts
@@ -5,7 +5,7 @@ import {KEY_REG, symbolsValidateMessage} from '../../validation-schema-compiler'
 export const entryName = () =>
     z
         .string()
-        .refine((value) => /^[^/]*$/.test(value), {
+        .refine((value) => !value.includes('/'), {
             message: 'String should not contain the symbol /',
         })
         .refine((value) => KEY_REG.test(value), {
