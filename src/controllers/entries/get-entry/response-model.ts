3ef3922afccdf6258e01a546ef7422a92aad0c4a
Vladimir Stepanenko <endurance@yandex-team.ru>
2025-08-25 18:07:54 +0300

    Add anotation to new controller

--- src/controllers/entries/get-entry/response-model.ts
+++ src/controllers/entries/get-entry/response-model.ts
@@ -2,6 +2,7 @@ import {AppContext} from '@gravity-ui/nodekit';
 
 import {z} from '../../../components/zod';
 import {EntryScope} from '../../../db/models/new/entry/types';
+import {RevisionAnnotationFields} from '../../../db/models/new/revision';
 import {GetEntryResult} from '../../../services/new/entry';
 import Utils from '../../../utils';
 
@@ -22,6 +23,11 @@ const schema = z
         tenantId: z.string().nullable(),
         data: z.record(z.string(), z.unknown()).nullable(),
         meta: z.record(z.string(), z.unknown()).nullable(),
+        annotation: z
+            .object({
+                [RevisionAnnotationFields.Description]: z.string().optional(),
+            })
+            .nullable(),
         hidden: z.boolean(),
         public: z.boolean(),
         workbookId: z.string().nullable(),
@@ -92,6 +98,7 @@ const format = (
         tenantId: entry.tenantId,
         data: revision.data,
         meta: revision.meta,
+        annotation: revision.annotation,
         hidden: entry.hidden,
         public: entry.public,
         workbookId: entry.workbookId ? Utils.encodeId(entry.workbookId) : null,
@@ -102,6 +109,7 @@ const format = (
         servicePlan: includeServicePlan ? servicePlan : undefined,
         tenantFeatures: includeTenantFeatures ? tenantFeatures : undefined,
         tenantSettings: includeTenantSettings ? tenantSettings : undefined,
+
         ...additionalFields,
     };
 };
