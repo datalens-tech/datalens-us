c4f8af67a662c2f486926ee85ac5b8bcefa93445
YankinA <yankin-anton@yandex-team.ru>
2025-09-10 16:47:40 +0300

    swap idx fields

--- src/db/migrations/20250910073942_add_user_settings_tenant_id_user_id_idx.ts
+++ src/db/migrations/20250910073942_add_user_settings_tenant_id_user_id_idx.ts
@@ -2,13 +2,13 @@ import type {Knex} from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     await knex.raw(
-        `CREATE UNIQUE INDEX CONCURRENTLY user_settings_tenant_id_user_id_idx ON user_settings (tenant_id, user_id);`,
+        `CREATE UNIQUE INDEX CONCURRENTLY user_settings_user_id_tenant_id_idx ON user_settings (user_id, tenant_id);`,
     );
 }
 
 export async function down(knex: Knex): Promise<void> {
     await knex.raw(`
-        DROP INDEX CONCURRENTLY user_settings_tenant_id_user_id_idx;
+        DROP INDEX CONCURRENTLY user_settings_user_id_tenant_id_idx;
     `);
 }
 
