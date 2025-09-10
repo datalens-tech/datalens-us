3385b70084534aeea03af3dfb25617d9d06ce700
YankinA <yankin-anton@yandex-team.ru>
2025-09-10 12:42:09 +0300

    add idx migrations

---
+++ src/db/migrations/20250910073942_add_user_settings_tenant_id_user_id_idx.ts
@@ -0,0 +1,17 @@
+import type {Knex} from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.raw(
+        `CREATE UNIQUE INDEX CONCURRENTLY user_settings_tenant_id_user_id_idx ON user_settings (tenant_id, user_id);`,
+    );
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.raw(`
+    DROP INDEX CONCURRENTLY IF EXISTS user_settings_tenant_id_user_id_idx;
+ `);
+}
+
+export const config = {
+    transaction: false,
+};
