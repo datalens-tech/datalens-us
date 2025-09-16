9a4cf14d1f9b9009137bea416415b5b230915815
Vladimir Stepanenko <endurance@yandex-team.ru>
2025-09-16 17:36:42 +0300

    Add filters to deleteEntry

---
+++ src/components/zod/custom-types/query-string-array.ts
@@ -0,0 +1,8 @@
+import {z} from 'zod';
+
+export const queryStringArray = () => {
+    return z
+        .union([z.string(), z.array(z.string())])
+        .transform((val) => (typeof val === 'string' ? [val] : val))
+        .optional();
+};
