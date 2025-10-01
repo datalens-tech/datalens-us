79663f8ca271521d58b47fe7185385abd628d118
Maksim Sitnikov <msitnikov1@yandex-team.ru>
2025-10-01 13:01:43 +0300

    Add links to getRelations (#359)

--- src/services/entry/actions/get-related-entries.ts
+++ src/services/entry/actions/get-related-entries.ts
@@ -105,7 +105,11 @@ export async function getRelatedEntries(
 
     const relatedEntriesQuery = Entry.query(getReplica(trx))
         .select([...RETURN_RELATION_COLUMNS, 'entries.created_at'])
-        .join('revisions', 'entries.savedId', 'revisions.revId')
+        .join(
+            'revisions',
+            raw('COALESCE(entries.published_id, entries.saved_id)'),
+            'revisions.revId',
+        )
         .whereIn('entries.entryId', entryIdList)
         .orderBy('entries.createdAt');
 
