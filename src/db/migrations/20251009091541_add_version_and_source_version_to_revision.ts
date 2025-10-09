438e9fdba8a3f8a17cee507d210396a055f53d0a
Vladimir Stepanenko <endurance@yandex-team.ru>
2025-10-09 12:21:32 +0300

    Add version to revision migration

--- src/db/migrations/20251009091541_add_version_and_source_version_to_revision.ts
+++ src/db/migrations/20251009091541_add_version_and_source_version_to_revision.ts
@@ -2,12 +2,14 @@ import type {Knex} from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     await knex.raw(`
-        ALTER TABLE revisions ADD COLUMN version INT2 DEFAULT 0;
+        ALTER TABLE revisions ADD COLUMN version INT2;
+        ALTER TABLE revisions ADD COLUMN source_version INT2;
     `);
 }
 
 export async function down(knex: Knex): Promise<void> {
     await knex.raw(`
         ALTER TABLE revisions DROP COLUMN version;
+        ALTER TABLE revisions DROP COLUMN source_version;
     `);
 }
