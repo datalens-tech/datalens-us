304420a02dec0c58e41b5b53660f3a47aa843e65
Maksim Sitnikov <msitnikov1@yandex-team.ru>
2025-11-17 19:46:28 +0300

    Licensing refactoring (#376)

--- src/db/models/new/tenant/presentations/tenant-with-current-limit.ts
+++ src/db/models/new/tenant/presentations/tenant-with-current-limit.ts
@@ -5,34 +5,76 @@ import {LicenseLimit, LicenseLimitColumn} from '../../license-limit';
 import {Tenant, TenantColumn} from '../index';
 
 export class TenantWithCurrentLimit extends Tenant {
-    static currentLimitsAlias = 'current_limits';
-
-    static getSelectQuery(trx: TransactionOrKnex, args?: {checkTimestamp?: RawBuilder | string}) {
+    static getSelectQuery(
+        trx: TransactionOrKnex,
+        args?: {minCreatorsLimitValue?: number; checkTimestamp?: RawBuilder | string},
+    ) {
+        const minCreatorsLimitValue = args?.minCreatorsLimitValue ?? 0;
         const checkTimestamp = args?.checkTimestamp ?? raw(CURRENT_TIMESTAMP);
 
         const query = Tenant.query(trx)
-            .with(this.currentLimitsAlias, (qb) => {
-                qb.select([LicenseLimitColumn.TenantId, LicenseLimitColumn.CreatorsLimitValue])
-                    .distinctOn(LicenseLimitColumn.TenantId)
-                    .from(LicenseLimit.tableName)
-                    .where(LicenseLimitColumn.StartedAt, '<=', checkTimestamp)
-                    .orderBy([
-                        {column: LicenseLimitColumn.TenantId},
-                        {column: LicenseLimitColumn.StartedAt, order: OrderBy.Desc},
-                    ]);
-            })
             .select(
                 `${Tenant.tableName}.*`,
-                `${this.currentLimitsAlias}.${LicenseLimitColumn.CreatorsLimitValue}`,
+                raw(`?? IS NOT NULL AND ?? > ?`, [
+                    `${Tenant.tableName}.${TenantColumn.TrialEndedAt}`,
+                    `${Tenant.tableName}.${TenantColumn.TrialEndedAt}`,
+                    raw(CURRENT_TIMESTAMP),
+                ]).as('trial_is_active'),
+                this.getCurrentLicenseLimitSubQuery({
+                    trx,
+                    minCreatorsLimitValue,
+                    checkTimestamp,
+                    inSelect: true,
+                }),
             )
-            .innerJoin(
-                this.currentLimitsAlias,
-                `${Tenant.tableName}.${TenantColumn.TenantId}`,
-                `${this.currentLimitsAlias}.${LicenseLimitColumn.TenantId}`,
+            .whereExists(
+                this.getCurrentLicenseLimitSubQuery({
+                    trx,
+                    minCreatorsLimitValue,
+                    checkTimestamp,
+                }),
             );
 
         return query as QueryBuilder<TenantWithCurrentLimit, TenantWithCurrentLimit[]>;
     }
 
+    private static getCurrentLicenseLimitSubQuery({
+        trx,
+        minCreatorsLimitValue,
+        checkTimestamp,
+        inSelect = false,
+    }: {
+        trx: TransactionOrKnex;
+        minCreatorsLimitValue: number;
+        checkTimestamp: RawBuilder | string;
+        inSelect?: boolean;
+    }) {
+        const query = LicenseLimit.query(trx)
+            .select(inSelect ? LicenseLimitColumn.CreatorsLimitValue : raw('1'))
+            .whereRaw('?? = ??', [
+                `${LicenseLimit.tableName}.${LicenseLimitColumn.TenantId}`,
+                `${Tenant.tableName}.${TenantColumn.TenantId}`,
+            ])
+            .andWhere(
+                `${LicenseLimit.tableName}.${LicenseLimitColumn.CreatorsLimitValue}`,
+                '>=',
+                minCreatorsLimitValue,
+            )
+            .andWhere(
+                `${LicenseLimit.tableName}.${LicenseLimitColumn.StartedAt}`,
+                '<=',
+                checkTimestamp,
+            )
+            .orderBy(`${LicenseLimit.tableName}.${LicenseLimitColumn.StartedAt}`, OrderBy.Desc)
+            .limit(1);
+
+        if (inSelect) {
+            query.as(LicenseLimitColumn.CreatorsLimitValue);
+        }
+
+        return query;
+    }
+
     [LicenseLimitColumn.CreatorsLimitValue]!: LicenseLimit[typeof LicenseLimitColumn.CreatorsLimitValue];
+    trialIsActive!: boolean;
 }
