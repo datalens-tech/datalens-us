1cbff115e1efb51216aa3a8f24787ae79908bd8c
Vladimir Stepanenko <endurance@yandex-team.ru>
2025-11-19 12:20:25 +0300

    Add tests

---
+++ src/tests/int/env/opensource/suites/entries/get-entries-relations.test.ts
@@ -0,0 +1,204 @@
+import request from 'supertest';
+
+import {app, auth} from '../../../platform/auth';
+import {createMockWorkbook, createMockWorkbookEntry} from '../../../platform/helpers';
+
+let workbookId: string;
+
+let connectionId: string;
+let datasetId: string;
+let widgetId: string;
+let dashId: string;
+
+const commonExpect = {
+    key: expect.any(String),
+    createdAt: expect.any(String),
+    meta: null,
+    public: false,
+    tenantId: 'org_test-tenant-id',
+    collectionId: null,
+    links: null,
+    isLocked: false,
+};
+
+describe('Get entries relations', () => {
+    test('[Setup test data] Create workbook and entries with links', async () => {
+        const workbook = await createMockWorkbook({title: 'My workbook'});
+
+        workbookId = workbook.workbookId;
+
+        const connectionEntry = await createMockWorkbookEntry({
+            name: 'Workbook connection',
+            workbookId: workbookId,
+            scope: 'connection',
+            type: 'connection-type',
+        });
+
+        connectionId = connectionEntry.entryId;
+
+        const datasetEntry = await createMockWorkbookEntry({
+            name: 'Workbook dataset',
+            workbookId: workbookId,
+            scope: 'dataset',
+            type: 'dataset-type',
+            links: {
+                [connectionId]: connectionId,
+            },
+        });
+
+        datasetId = datasetEntry.entryId;
+
+        const widgetEntry = await createMockWorkbookEntry({
+            name: 'Workbook widget',
+            workbookId: workbookId,
+            scope: 'widget',
+            type: 'widget-type',
+        });
+
+        widgetId = widgetEntry.entryId;
+
+        const dashEntry = await createMockWorkbookEntry({
+            name: 'Workbook dash',
+            workbookId: workbookId,
+            scope: 'dash',
+            type: 'dash-type',
+            links: {
+                [widgetId]: widgetId,
+                [datasetId]: datasetId,
+            },
+        });
+
+        dashId = dashEntry.entryId;
+    });
+
+    test('Get children entries relations by entryIds', async () => {
+        const response = await auth(request(app).post('/v1/get-entries-relations'))
+            .send({
+                entryIds: [dashId],
+            })
+            .expect(200);
+
+        expect(response.body).toStrictEqual({
+            relations: [
+                {
+                    ...commonExpect,
+                    entryId: datasetId,
+                    scope: 'dataset',
+                    type: 'dataset-type',
+                    workbookId: workbookId,
+                    links: {
+                        [connectionId]: connectionId,
+                    },
+                },
+                {
+                    ...commonExpect,
+                    entryId: widgetId,
+                    scope: 'widget',
+                    type: 'widget-type',
+                    workbookId: workbookId,
+                },
+            ],
+        });
+    });
+
+    test('Get parent entries relations by entryIds', async () => {
+        const response = await auth(request(app).post('/v1/get-entries-relations'))
+            .send({
+                entryIds: [connectionId, widgetId],
+                searchDirection: 'parents',
+            })
+            .expect(200);
+
+        expect(response.body).toStrictEqual({
+            relations: [
+                {
+                    ...commonExpect,
+                    entryId: datasetId,
+                    scope: 'dataset',
+                    type: 'dataset-type',
+                    workbookId: workbookId,
+                    links: {
+                        [connectionId]: connectionId,
+                    },
+                },
+                {
+                    ...commonExpect,
+                    entryId: dashId,
+                    scope: 'dash',
+                    type: 'dash-type',
+                    workbookId: workbookId,
+                    links: {
+                        [widgetId]: widgetId,
+                        [datasetId]: datasetId,
+                    },
+                },
+            ],
+        });
+    });
+
+    test('Get entries relations filtered by scope', async () => {
+        const response = await auth(request(app).post('/v1/get-entries-relations'))
+            .send({
+                entryIds: [dashId],
+                scope: 'widget',
+            })
+            .expect(200);
+
+        expect(response.body).toStrictEqual({
+            relations: [
+                {
+                    ...commonExpect,
+                    entryId: widgetId,
+                    scope: 'widget',
+                    type: 'widget-type',
+                    workbookId: workbookId,
+                },
+            ],
+        });
+    });
+
+    test('Get entries relations with pagination', async () => {
+        const response1 = await auth(request(app).post('/v1/get-entries-relations'))
+            .send({
+                entryIds: [dashId],
+                limit: 1,
+            })
+            .expect(200);
+
+        expect(response1.body).toStrictEqual({
+            relations: [
+                {
+                    ...commonExpect,
+                    entryId: datasetId,
+                    scope: 'dataset',
+                    type: 'dataset-type',
+                    workbookId: workbookId,
+                    links: {
+                        [connectionId]: connectionId,
+                    },
+                },
+            ],
+            nextPageToken: expect.any(String),
+        });
+
+        const response2 = await auth(request(app).post('/v1/get-entries-relations'))
+            .send({
+                entryIds: [dashId],
+                limit: 1,
+                pageToken: response1.body.nextPageToken,
+            })
+            .expect(200);
+
+        expect(response2.body).toStrictEqual({
+            relations: [
+                {
+                    ...commonExpect,
+                    entryId: widgetId,
+                    scope: 'widget',
+                    type: 'widget-type',
+                    workbookId: workbookId,
+                },
+            ],
+        });
+    });
+});
