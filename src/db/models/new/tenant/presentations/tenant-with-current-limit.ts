09d00848552444356e5f9e4b9023f43050923f44
Maksim Sitnikov <msitnikov1@yandex-team.ru>
2025-11-25 02:35:01 +0300

    Fix

--- src/db/models/new/tenant/presentations/tenant-with-current-limit.ts
+++ src/db/models/new/tenant/presentations/tenant-with-current-limit.ts
@@ -1,18 +1,26 @@
 import {QueryBuilder, RawBuilder, TransactionOrKnex, raw} from 'objection';
 
 import {CURRENT_TIMESTAMP, OrderBy} from '../../../../../const';
-import {LicenseLimit, LicenseLimitColumn, LicenseLimitColumnRaw} from '../../license-limit';
+import {LicenseLimit, LicenseLimitColumn} from '../../license-limit';
 import {Tenant, TenantColumn} from '../index';
 
 export class TenantWithCurrentLimit extends Tenant {
-    static getSelectQuery(
-        trx: TransactionOrKnex,
-        args?: {minCreatorsLimitValue?: number; checkTimestamp?: RawBuilder | string},
-    ) {
-        const minCreatorsLimitValue = args?.minCreatorsLimitValue ?? 0;
+    protected static currentLimitsAlias = 'current_limits';
+
+    static getSelectQuery(trx: TransactionOrKnex, args?: {checkTimestamp?: RawBuilder | string}) {
         const checkTimestamp = args?.checkTimestamp ?? raw(CURRENT_TIMESTAMP);
 
         const query = Tenant.query(trx)
+            .with(this.currentLimitsAlias, (qb) => {
+                qb.select([LicenseLimitColumn.TenantId, LicenseLimitColumn.CreatorsLimitValue])
+                    .distinctOn(LicenseLimitColumn.TenantId)
+                    .from(LicenseLimit.tableName)
+                    .where(LicenseLimitColumn.StartedAt, '<=', checkTimestamp)
+                    .orderBy([
+                        {column: LicenseLimitColumn.TenantId},
+                        {column: LicenseLimitColumn.StartedAt, order: OrderBy.Desc},
+                    ]);
+            })
             .select(
                 `${Tenant.tableName}.*`,
                 raw(`?? IS NOT NULL AND ?? > ?`, [
@@ -20,42 +28,15 @@ export class TenantWithCurrentLimit extends Tenant {
                     `${Tenant.tableName}.${TenantColumn.TrialEndedAt}`,
                     checkTimestamp,
                 ]).as('trial_is_active'),
-                this.getCurrentLicenseLimitSubQuery({
-                    trx,
-                    checkTimestamp,
-                }).as(LicenseLimitColumnRaw.CreatorsLimitValue),
+                `${this.currentLimitsAlias}.${LicenseLimitColumn.CreatorsLimitValue}`,
             )
-            .whereRaw(`(??) >= ?`, [
-                this.getCurrentLicenseLimitSubQuery({
-                    trx,
-                    checkTimestamp,
-                }),
-                minCreatorsLimitValue,
-            ]);
+            .innerJoin(
+                this.currentLimitsAlias,
+                `${Tenant.tableName}.${TenantColumn.TenantId}`,
+                `${this.currentLimitsAlias}.${LicenseLimitColumn.TenantId}`,
+            );
 
         return query as QueryBuilder<TenantWithCurrentLimit, TenantWithCurrentLimit[]>;
-    }
-
-    private static getCurrentLicenseLimitSubQuery({
-        trx,
-        checkTimestamp,
-    }: {
-        trx: TransactionOrKnex;
-        checkTimestamp: RawBuilder | string;
-    }) {
-        return LicenseLimit.query(trx)
-            .select(LicenseLimitColumn.CreatorsLimitValue)
-            .whereRaw('?? = ??', [
-                `${LicenseLimit.tableName}.${LicenseLimitColumn.TenantId}`,
-                `${Tenant.tableName}.${TenantColumn.TenantId}`,
-            ])
-            .andWhere(
-                `${LicenseLimit.tableName}.${LicenseLimitColumn.StartedAt}`,
-                '<=',
-                checkTimestamp,
-            )
-            .orderBy(`${LicenseLimit.tableName}.${LicenseLimitColumn.StartedAt}`, OrderBy.Desc)
-            .limit(1);
     }
 
     [LicenseLimitColumn.CreatorsLimitValue]!: LicenseLimit[typeof LicenseLimitColumn.CreatorsLimitValue];
