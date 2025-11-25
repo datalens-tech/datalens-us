2e4c5ee122082a802f3090acba368e0a374042f9
Maksim Sitnikov <msitnikov1@yandex-team.ru>
2025-11-25 11:18:21 +0300

    Fix

--- src/db/models/new/tenant/presentations/tenant-with-current-limit.ts
+++ src/db/models/new/tenant/presentations/tenant-with-current-limit.ts
@@ -5,23 +5,61 @@ import {LicenseLimit, LicenseLimitColumn} from '../../license-limit';
 import {Tenant, TenantColumn} from '../index';
 
 export class TenantWithCurrentLimit extends Tenant {
+    protected static allIncreasedLimitsAlias = 'all_increased_limits';
     protected static currentLimitsAlias = 'current_limits';
 
-    static getSelectQuery(trx: TransactionOrKnex, args?: {checkTimestamp?: RawBuilder | string}) {
+    static getSelectQuery(
+        trx: TransactionOrKnex,
+        args?: {minCreatorsLimitValue?: number; checkTimestamp?: RawBuilder | string},
+    ) {
+        const minCreatorsLimitValue = args?.minCreatorsLimitValue ?? 0;
         const checkTimestamp = args?.checkTimestamp ?? raw(CURRENT_TIMESTAMP);
 
         const query = Tenant.query(trx)
-            .with(this.currentLimitsAlias, (qb) => {
-                qb.select([LicenseLimitColumn.TenantId, LicenseLimitColumn.CreatorsLimitValue])
-                    .distinctOn(LicenseLimitColumn.TenantId)
+            .with(this.allIncreasedLimitsAlias, (qb) => {
+                qb.select([`${LicenseLimit.tableName}.${LicenseLimitColumn.TenantId}`])
                     .from(LicenseLimit.tableName)
-                    .where(LicenseLimitColumn.StartedAt, '<=', checkTimestamp)
+                    .where(
+                        `${LicenseLimit.tableName}.${LicenseLimitColumn.StartedAt}`,
+                        '<=',
+                        checkTimestamp,
+                    )
+                    .andWhere(
+                        `${LicenseLimit.tableName}.${LicenseLimitColumn.CreatorsLimitValue}`,
+                        '>=',
+                        minCreatorsLimitValue,
+                    )
+                    .groupBy(LicenseLimitColumn.TenantId);
+            })
+            .with(this.currentLimitsAlias, (qb) => {
+                qb.select([
+                    `${LicenseLimit.tableName}.${LicenseLimitColumn.TenantId}`,
+                    `${LicenseLimit.tableName}.${LicenseLimitColumn.CreatorsLimitValue}`,
+                ])
+                    .distinctOn(`${LicenseLimit.tableName}.${LicenseLimitColumn.TenantId}`)
+                    .from(LicenseLimit.tableName)
+                    .whereRaw(`?? IN (SELECT ?? FROM ??)`, [
+                        `${LicenseLimit.tableName}.${LicenseLimitColumn.TenantId}`,
+                        LicenseLimitColumn.TenantId,
+                        raw(this.allIncreasedLimitsAlias),
+                    ])
+                    .where(
+                        `${LicenseLimit.tableName}.${LicenseLimitColumn.StartedAt}`,
+                        '<=',
+                        checkTimestamp,
+                    )
                     .orderBy([
-                        {column: LicenseLimitColumn.TenantId},
-                        {column: LicenseLimitColumn.StartedAt, order: OrderBy.Desc},
+                        {
+                            column: `${LicenseLimit.tableName}.${LicenseLimitColumn.TenantId}`,
+                            order: OrderBy.Asc,
+                        },
+                        {
+                            column: `${LicenseLimit.tableName}.${LicenseLimitColumn.StartedAt}`,
+                            order: OrderBy.Desc,
+                        },
                     ]);
             })
-            .select(
+            .select([
                 `${Tenant.tableName}.*`,
                 raw(`?? IS NOT NULL AND ?? > ?`, [
                     `${Tenant.tableName}.${TenantColumn.TrialEndedAt}`,
@@ -29,11 +67,16 @@ export class TenantWithCurrentLimit extends Tenant {
                     checkTimestamp,
                 ]).as('trial_is_active'),
                 `${this.currentLimitsAlias}.${LicenseLimitColumn.CreatorsLimitValue}`,
-            )
+            ])
             .innerJoin(
                 this.currentLimitsAlias,
                 `${Tenant.tableName}.${TenantColumn.TenantId}`,
                 `${this.currentLimitsAlias}.${LicenseLimitColumn.TenantId}`,
+            )
+            .where(
+                `${this.currentLimitsAlias}.${LicenseLimitColumn.CreatorsLimitValue}`,
+                '>=',
+                minCreatorsLimitValue,
             );
 
         return query as QueryBuilder<TenantWithCurrentLimit, TenantWithCurrentLimit[]>;
