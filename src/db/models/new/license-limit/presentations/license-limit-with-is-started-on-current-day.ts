944de2f216ded1bbca3a9d5da10da46046ace8cf
Maksim Sitnikov <msitnikov1@yandex-team.ru>
2025-11-27 14:04:54 +0300

    Licensing fixes 2

--- src/db/models/new/license-limit/presentations/license-limit-with-is-started-on-current-day.ts
+++ src/db/models/new/license-limit/presentations/license-limit-with-is-started-on-current-day.ts
@@ -1,7 +1,7 @@
 import {QueryBuilder, TransactionOrKnex, raw} from 'objection';
 
 import {CURRENT_TIMESTAMP} from '../../../../../const';
-import {LicenseLimit, LicenseLimitColumnRaw} from '../index';
+import {LicenseLimit, LicenseLimitColumn} from '../index';
 
 export class LicenseLimitWithIsStartedOnCurrentDay extends LicenseLimit {
     static get selectedColumns() {
@@ -9,7 +9,7 @@ export class LicenseLimitWithIsStartedOnCurrentDay extends LicenseLimit {
             '*',
             raw(
                 `DATE_TRUNC('day', ?? AT TIME ZONE 'Europe/Moscow') = DATE_TRUNC('day', ? AT TIME ZONE 'Europe/Moscow')`,
-                [LicenseLimitColumnRaw.StartedAt, raw(CURRENT_TIMESTAMP)],
+                [LicenseLimitColumn.StartedAt, raw(CURRENT_TIMESTAMP)],
             ).as('is_started_on_current_day'),
         ];
     }
