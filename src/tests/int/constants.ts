2104e6a22a858eb716e6c1fa31424b0d93f113e1
Samat Jorobekov <samat1337@yandex-team.ru>
2025-11-27 18:39:58 +0300

    Enable LicensingModeEnabled in tests

--- src/tests/int/constants.ts
+++ src/tests/int/constants.ts
@@ -5,3 +5,4 @@ export const testUserId = 'test-user-id';
 export const testUserLogin = 'test-user-login';
 
 export const testOtherUserId = 'test-other-user-id';
+export const testOtherUserLogin = 'test-other-user-login';
