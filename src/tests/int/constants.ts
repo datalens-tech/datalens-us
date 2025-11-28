7d9a5d2e3c79fb0f6e32dff3599081d3a2aaa251
Samat Jorobekov <samat1337@yandex-team.ru>
2025-11-28 13:58:20 +0300

    Add default users

--- src/tests/int/constants.ts
+++ src/tests/int/constants.ts
@@ -6,3 +6,16 @@ export const testUserLogin = 'test-user-login';
 
 export const testOtherUserId = 'test-other-user-id';
 export const testOtherUserLogin = 'test-other-user-login';
+
+export const testOtherUserId2 = 'test-other-user-id-2';
+export const testOtherUserLogin2 = 'test-other-user-login-2';
+
+export const testOtherUserId3 = 'test-other-user-id-3';
+export const testOtherUserLogin3 = 'test-other-user-login-3';
+
+export const testUsers = [
+    [testUserId, testUserLogin],
+    [testOtherUserId, testOtherUserLogin],
+    [testOtherUserId2, testOtherUserLogin2],
+    [testOtherUserId3, testOtherUserLogin3],
+];
