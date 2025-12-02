83da540fcb731412bb6a07432c4d5a7a3ce096e9
Sergei Mulyk <s-mulyk@yandex-team.ru>
2025-12-02 15:29:20 +0300

    Subscription model (#388)

---
+++ src/db/models/new/subscription-recipients/index.ts
@@ -0,0 +1,29 @@
+import {Model} from '../../..';
+
+import {SubscriptionRecipientTransportType} from './types';
+
+export const SubscriptionRecipientColumn = {
+    SubscriptionRecipientId: 'subscriptionRecipientId',
+    SubscriptionId: 'subscriptionId',
+    UserId: 'userId',
+    transport: 'transport',
+    CreatedBy: 'createdBy',
+    CreatedAt: 'createdAt',
+} as const;
+
+export class SubscriptionRecipient extends Model {
+    static get tableName() {
+        return 'subscription_recipients';
+    }
+
+    static get idColumn() {
+        return SubscriptionRecipientColumn.SubscriptionRecipientId;
+    }
+
+    [SubscriptionRecipientColumn.SubscriptionRecipientId]!: string;
+    [SubscriptionRecipientColumn.SubscriptionId]!: string;
+    [SubscriptionRecipientColumn.UserId]!: string;
+    [SubscriptionRecipientColumn.transport]!: SubscriptionRecipientTransportType;
+    [SubscriptionRecipientColumn.CreatedBy]!: string;
+    [SubscriptionRecipientColumn.CreatedAt]!: string;
+}
