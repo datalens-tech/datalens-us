cb8f33fbd6181f84ce1707058090dc7ea4ff7a99
Evgeniy Shangin <shangin@yandex-team.ru>
2025-12-10 12:06:01 +0300

    Review fixes

--- src/components/temporal/init-temporal.ts
+++ src/components/temporal/init-temporal.ts
@@ -7,7 +7,5 @@ export const initTemporal = ({ctx}: InitTemporalDeps, options?: InitTemporalOpti
 
     initWorkers({ctx}, options).catch((error) => {
         ctx.logError('TEMPORAL_WORKER_FAIL', error);
-
-        process.exit(1);
     });
 };
