c0190faed107e1ec7fb575ed216118062e61c546
Sergei Mulyk <s-mulyk@yandex-team.ru>
2025-12-11 16:27:43 +0300

    improved

--- src/db/migrations/20251211094928_remove_subscription_recipient_id_and_make_composite_pk.ts
+++ src/db/migrations/20251211094928_remove_subscription_recipient_id_and_make_composite_pk.ts
@@ -2,9 +2,9 @@ import type {Knex} from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     await knex.raw(`
+        DROP INDEX subscription_recipients_subscription_id_user_id_transport_unique_idx;
         ALTER TABLE subscription_recipients DROP CONSTRAINT subscription_recipients_pkey;
         ALTER TABLE subscription_recipients DROP COLUMN subscription_recipient_id;
-        DROP INDEX IF EXISTS subscription_recipients_subscription_id_user_id_transport_unique_idx;
 
         ALTER TABLE subscription_recipients ADD PRIMARY KEY (subscription_id, user_id, transport);
     `);
