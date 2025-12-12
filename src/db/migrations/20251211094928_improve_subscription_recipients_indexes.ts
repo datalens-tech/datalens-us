7ac6ec5493778101f2f98f1917c97a4af8fe47bb
Sergei Mulyk <s-mulyk@yandex-team.ru>
2025-12-12 11:32:08 +0300

    New subscription recipients table primary key (#395)

---
+++ src/db/migrations/20251211094928_improve_subscription_recipients_indexes.ts
@@ -0,0 +1,23 @@
+import type {Knex} from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.raw(`
+        DROP INDEX subscription_recipients_subscription_id_user_id_transport_unique_idx;
+        ALTER TABLE subscription_recipients DROP CONSTRAINT subscription_recipients_pkey;
+        ALTER TABLE subscription_recipients DROP COLUMN subscription_recipient_id;
+
+        ALTER TABLE subscription_recipients ADD PRIMARY KEY (subscription_id, user_id, transport);
+        CREATE INDEX subscription_recipients_user_id_transport_idx ON subscription_recipients(user_id, transport);
+    `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.raw(`
+        DROP INDEX subscription_recipients_user_id_transport_idx;
+        ALTER TABLE subscription_recipients DROP CONSTRAINT subscription_recipients_pkey;
+
+        ALTER TABLE subscription_recipients ADD COLUMN subscription_recipient_id BIGINT NOT NULL DEFAULT get_id();
+        ALTER TABLE subscription_recipients ADD PRIMARY KEY (subscription_recipient_id);
+        CREATE UNIQUE INDEX subscription_recipients_subscription_id_user_id_transport_unique_idx ON subscription_recipients(subscription_id, user_id, transport);
+    `);
+}
