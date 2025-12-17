b943799344e8807f4d420b9fb79cf16a78efd51a
Samat Jorobekov <samat1337@yandex-team.ru>
2025-12-17 17:40:40 +0300

    Fixes

--- src/configs/common.ts
+++ src/configs/common.ts
@@ -1,7 +1,12 @@
 import {AuthPolicy} from '@gravity-ui/expresskit';
 import {AppConfig} from '@gravity-ui/nodekit';
 
-import {APP_NAME, US_DYNAMIC_MASTER_TOKEN_HEADER, US_MASTER_TOKEN_HEADER} from '../const';
+import {
+    APP_NAME,
+    DL_SERVICE_USER_ACCESS_TOKEN,
+    US_DYNAMIC_MASTER_TOKEN_HEADER,
+    US_MASTER_TOKEN_HEADER,
+} from '../const';
 import {getEnvCert, getEnvTokenVariable, getEnvVariable, isTrueArg} from '../utils/env-utils';
 
 const isZitadelEnabled = isTrueArg(getEnvVariable('ZITADEL'));
@@ -24,7 +29,16 @@ export default {
 
     appAuthPolicy: isAuthServiceEnabled ? AuthPolicy.required : AuthPolicy.disabled,
 
-    appSensitiveKeys: [US_MASTER_TOKEN_HEADER, US_DYNAMIC_MASTER_TOKEN_HEADER],
+    appSensitiveKeys: [
+        US_MASTER_TOKEN_HEADER,
+        US_DYNAMIC_MASTER_TOKEN_HEADER,
+        DL_SERVICE_USER_ACCESS_TOKEN,
+    ],
+    appSensitiveHeaders: [
+        US_MASTER_TOKEN_HEADER,
+        US_DYNAMIC_MASTER_TOKEN_HEADER,
+        DL_SERVICE_USER_ACCESS_TOKEN,
+    ],
 
     // zitadel
     zitadelEnabled: isZitadelEnabled,
--- src/configs/opensource/common.ts
+++ src/configs/opensource/common.ts
@@ -1,7 +1,6 @@
 import {AppConfig} from '@gravity-ui/nodekit';
 
 import {Feature, FeaturesConfig} from '../../components/features/types';
-import {DL_SERVICE_USER_ACCESS_TOKEN} from '../../const';
 import {isTrueArg} from '../../utils/env-utils';
 
 export const features: FeaturesConfig = {
@@ -18,7 +17,5 @@ export const features: FeaturesConfig = {
 };
 
 export default {
-    appSensitiveKeys: [DL_SERVICE_USER_ACCESS_TOKEN],
-    appSensitiveHeaders: [DL_SERVICE_USER_ACCESS_TOKEN],
     features,
 } as Partial<AppConfig>;
